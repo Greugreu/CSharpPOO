8e523dd412d0d22c0a180be62a17b717056a1e35
Greugreu <clarke.victor76@hotmail.fr>
2020-04-28 15:31:47 +0200

    commit

---
+++ HelloPOO2/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloPOO2
+{
+    class Animal // <- Class name
+    {
+        /*
+            Personal notes on syntax:
+            To instantiate a new object, we have to use the class name before the object name
+            cf. Arrows
+            
+            Class Members
+            Fields and methods inside classes are often referred to as "Class Members"
+         */
+        
+        public string name = "Cat"; // field
+        public int maxRunningSpeed = 200; // field
+        public void fullThrottle() // method
+        {
+            Console.WriteLine("The cat is going as fast as it can!");
+        }
+        
+        // we also can leave fields blank and modify them when creating objects
+        public string race;
+        public int legs;
+
+    }
+}
--- HelloPOO2/Program.cs
+++ HelloPOO2/Program.cs
@@ -1,35 +1,27 @@
 ﻿using System;
 
-/*namespace HelloPOO2
+namespace HelloPOO2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Animal myPet = new Animal(); // <- Animal class name have to be called before Obj name
+            Console.WriteLine(myPet.name);
+            Console.WriteLine(myPet.maxRunningSpeed);
+            myPet.fullThrottle();
+            myPet.race = "Main Coon";
+            myPet.legs = 4;
+            Console.WriteLine(myPet.race);
+            Console.WriteLine(myPet.legs);
         }
     }
-}*/
+}
 
 /*
     Create a class / object
     As PHP, to create a class use class keyword
-    Objects refers to an instenciation 
- */
-
-class Animal // <- Class name
-{
-    string name = "Cat";
-
-    static void Main(string[] args)
-    {
-        Animal myPet = new Animal(); // <- Animal class name have to be called before Obj name
-        Console.WriteLine(myPet.name);
-    }
+    Objects refers to an instantiation 
     
-    /*
-        Personal notes on syntax:
-        To instentiate a new object, we have to use the class name before the object name
-        cf. Arrows
-     */
-}
+    We created a new Animal.cs file that content our new class.
+ */
