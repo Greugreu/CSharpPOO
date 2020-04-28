c8f14f5b5f7d7953cfb1e0ee748f8a638d50129a
Greugreu <clarke.victor76@hotmail.fr>
2020-04-28 14:58:43 +0200

    commit

---
+++ HelloPOO2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/*namespace HelloPOO2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}*/
+
+/*
+    Create a class / object
+    As PHP, to create a class use class keyword
+    Objects refers to an instenciation 
+ */
+
+class Animal // <- Class name
+{
+    string name = "Cat";
+
+    static void Main(string[] args)
+    {
+        Animal myPet = new Animal(); // <- Animal class name have to be called before Obj name
+        Console.WriteLine(myPet.name);
+    }
+    
+    /*
+        Personal notes on syntax:
+        To instentiate a new object, we have to use the class name before the object name
+        cf. Arrows
+     */
+}
