d9357c015fa3a365750e6934114959affd91bd0f
Greugreu <clarke.victor76@hotmail.fr>
2020-04-29 11:19:25 +0200

    Commit

---
+++ HelloPOO3/Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HelloPOO3
+{
+    public class Animal
+    {
+        // Class Constructors
+        // Called when created, does the same things than PHP.
+
+        public string race;
+        
+        //Constructor creation
+        public Animal(string raceType)
+        {
+            race = raceType; // Set the inital value for type
+        }
+        
+        // The constructor name must match the class name, it cannot have a return type (like void or int)
+        
+        // CONSTRUCTOR PARAMETERS
+        /*
+            Like methods, constructor car take parameters
+            
+            public name(string varName)
+            {
+                var = varName
+            }
+            
+            so previously constructor was
+            
+            public Animal()
+            {
+                race = "cat";
+            }
+            
+            We changed for the above.
+         */
+    }
+}
---
+++ HelloPOO3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloPOO3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal Cat = new Animal("Cat"); // Create an Animal object called Cat (Constructor is called at the same time)
+            Console.WriteLine(Cat.race);
+            
+        }
+    }
+}
